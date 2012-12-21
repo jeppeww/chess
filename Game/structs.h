a75757b11af0c8adc1b61169f9bde4e319237baf
Pungsnigel <JesperT.Persson@gmail.com>
2012-12-22 00:41:54 +0100

    Created file for helper structs and enums

---
+++ Game/structs.h
@@ -0,0 +1,22 @@
+//
+//  File:       structs.h
+//  Author:     Jesper Persson & Jesper Wikström
+//              All code is our own except where credited to others.
+//
+//	Copyright (c) 2012 by Jesper Persson & Jesper Wikström. All Rights Reserved.
+//  Date: 		21/12-2012
+//
+//  Description: 
+//
+
+
+struct Point
+{
+	int m_X, m_Y;
+};
+
+enum Players
+{
+	BLACK = 1,
+	WHITE = -1
+};
