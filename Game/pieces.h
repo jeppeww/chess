66b189cc8af10d96fe98143ca31f575856ad4e8f
Pungsnigel <JesperT.Persson@gmail.com>
2012-12-22 00:55:39 +0100

    Updated header for pieces

--- Game/pieces.h
+++ Game/pieces.h
@@ -16,10 +16,25 @@ class Piece
 {
 
 public:
+	Piece (Point _Position, Players _Owner, Board* _Board)
+	{
+		m_Position = _Position;
+		m_Owner = _Owner;
+		m_Board = _Board;
+	}
+	~Piece(){}
 
+	Point getPosition()
+	{
+		return m_Position;
+	}
 
+	void setPosition(Point _Position);
+	virtual bool canMove (Point _Destination) = 0;
+	void kill();
 
 private:
-
+	Point m_Position;
 	Board* m_Board;
+	Players m_Owner;
 }
